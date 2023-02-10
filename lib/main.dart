48f4108b53b859071374701698ee185a64f0ec29
Somnath Mistry <somnath222666@gmail.com>
2023-02-10 13:21:28 +0530

    10-02-2023

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
       title: 'Meesho Clone',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
